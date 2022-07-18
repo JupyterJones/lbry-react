09b42feedab4e01ebf15634aefa2a7f0c981f545
JupyterJones <jack.northrup.ph@gmail.com>
2022-07-18 10:42:46 +0800

    update

---
+++ public/heading.php
@@ -0,0 +1,8 @@
+<div class="heading shad">
+<h1>INFO CENTER</h1>
+<p>
+<strong>Info Center</strong> has pages, examples, hints, and snippets on the various topics in the menu above. Explore and enjoy.
+</p>
+
+
+</div>
