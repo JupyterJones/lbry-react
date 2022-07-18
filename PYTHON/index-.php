9ac27461fd95d7cae2630c67901a12dd53bd5836
JupyterJones <jack.northrup.ph@gmail.com>
2022-07-18 22:44:37 +0800

    working on directory structure
    working on directory structure

---
+++ PYTHON/index-.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Create an FFMPEG interface with Python tkinter</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../mystyle.css">
+    </head>
+    <body>
+        <?php include '../menu.php'; ?>
+       
+
+
+<div id="wrapper">
+    
+    </div>
+       <footer><h2>References and Help</h2>
+    <ul>
+
+        <li><a href="https://pythonexamples.org/">Python Examples (pythonexamples.orgS) </a></li>
+    </ul>
+    
+    
+    
+    </footer> 
+    
+        <script src="" async defer></script>
+    </body>
+</html>
